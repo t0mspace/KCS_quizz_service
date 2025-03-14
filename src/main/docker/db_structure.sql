d8b5bfc6a3c60d6a007335e6bc2ace99fd4670e7
Thomas Brenard <thomasx2005@gmail.com>
2025-03-14 15:34:51 +0100

    some adds

--- src/main/docker/db_structure.sql
+++ src/main/docker/db_structure.sql
@@ -1,44 +1,39 @@
 
-DROP SCHEMA IF EXISTS aubay CASCADE;
-CREATE SCHEMA IF NOT EXISTS aubay;
+DROP SCHEMA IF EXISTS public CASCADE;
+CREATE SCHEMA IF NOT EXISTS public;
 
 --
 --
--- CREATE TABLE aubay.role
--- (
---     id integer UNIQUE,
---     name character varying(50) NOT NULL,
---     PRIMARY KEY (name)
--- );
---
--- INSERT INTO aubay.role(id, name)
--- VALUES (1,'ROLE_ADMIN');
---
--- INSERT INTO aubay.role(name)
--- VALUES ('ROLE_USER');
---
---
--- CREATE TABLE aubay.user (
---                         id integer NOT NULL,
---                         "username" varchar(250) UNIQUE NOT NULL,
---                         "id_role" integer NOT NULL,
---                         CONSTRAINT fk_id_role FOREIGN KEY (id_role)
---                             REFERENCES aubay.role (id),
---                         "email" varchar(250) UNIQUE NOT NULL,
---                         "token" varchar(250),
---                         password character varying(60) NOT NULL
--- );
+ CREATE TABLE public.role
+ (
+     id INT primary key GENERATED ALWAYS AS IDENTITY,
+     name character varying(50) NOT NULL,
+     PRIMARY KEY (name)
 
--- ALTER SEQUENCE user_id_seq OWNED BY user.id;
+     CONSTRAINT role_name_key UNIQUE (name),
+     CONSTRAINT role_pkey PRIMARY KEY (id)
+ );
 
--- ALTER TABLE IF EXISTS public.user OWNER to postgres;
--- CREATE TABLE "role" (
---     "id" integer NOT NULL DEFAULT nextval('role_id_seq'),
---    "name" varchar(250) UNIQUE NOT NULL
--- );
 
--- ALTER SEQUENCE role_id_seq OWNED BY role.id;
--- ALTER TABLE IF EXISTS public.role OWNER to postgres;
+--
+ INSERT INTO public.role(id, name)
+ VALUES (1,'ROLE_ADMIN');
+--
+ INSERT INTO public.role(name)
+ VALUES ('ROLE_USER');
+--
+--
+ CREATE TABLE public.user (
+         id INT primary key GENERATED ALWAYS AS IDENTITY,
+         username varchar(250) UNIQUE NOT NULL,
+         id_role integer NOT NULL,
+         CONSTRAINT fk_id_role FOREIGN KEY (id_role)
+             REFERENCES public.role (id),
+         email varchar(250) UNIQUE NOT NULL,
+         token varchar(250),
+         password character varying(60) NOT NULL
+ );
+
 
 
 
@@ -47,11 +42,11 @@ CREATE SCHEMA IF NOT EXISTS aubay;
 -- INSERT INTO aubay.user (id,username, id_role, email, token, password) VALUES (3,'aeinstein', 1, 'aeinstein@gmail.com', null, '$2a$12$RkcdJn2kLrAS9fmvDv/CWehqID8nB3XBWXOtazhQ2PY1ZFwDB3L76');
 --
 
--- CREATE TYPE "test_status" AS ENUM (
---   'draft',
---   'ready_to_use',
---   'archived'
--- );
+ CREATE TYPE "test_status" AS ENUM (
+   'draft',
+   'ready_to_use',
+   'archived'
+ );
 
 -- CREATE TABLE "test" (
 --   "id" SERIAL PRIMARY KEY,
@@ -65,59 +60,78 @@ CREATE SCHEMA IF NOT EXISTS aubay;
 --   "updated_by" integer
 -- );
 
-CREATE TABLE aubay.subject (
+
+CREATE TABLE public.subject (
+    id INT primary key GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(250) NOT NULL UNIQUE
+);
+
+CREATE TABLE public.quizz (
+                              id INT primary key GENERATED ALWAYS AS IDENTITY,
+                              id_subject INTEGER REFERENCES public.subject (id),
+                              name varchar(250) UNIQUE NOT NULL,
+                              description TEXT
+);
+
+--
+ CREATE TABLE "quizz_test" (
    id INT primary key GENERATED ALWAYS AS IDENTITY,
-   name VARCHAR(250) NOT NULL UNIQUE
+   id_quiz int NOT NULL,
+   id_test int NOT NULL
+ );
+
+
+
+CREATE TABLE public.question (
+     id INT primary key GENERATED ALWAYS AS IDENTITY,
+     id_quizz INTEGER REFERENCES public.quizz (id),
+     content TEXT
 );
 
 
-CREATE TABLE aubay.quizz (
-  id INT primary key GENERATED ALWAYS AS IDENTITY,
-  id_subject INTEGER REFERENCES aubay.subject (id),
-  name varchar(250) UNIQUE NOT NULL,
-   description TEXT
-);
---
--- CREATE TABLE "quiz_test" (
---   "id" SERIAL PRIMARY KEY,
---   "id_quiz" int NOT NULL,
---   "id_test" int NOT NULL
--- );
-
-
-
-CREATE TABLE aubay.question (
-  "id" INT GENERATED ALWAYS AS IDENTITY,
-  "id_quiz" integer,
-  "content" TEXT
+CREATE TABLE public.answer (
+   id INT GENERATED ALWAYS AS IDENTITY,
+   id_question integer,
+   content TEXT,
+   weighting integer,
+   is_correct bool NOT NULL
 );
 
-CREATE TABLE aubay.answer (
-  "id" INT GENERATED ALWAYS AS IDENTITY,
-  "id_question" integer NOT NULL,
-  "content" TEXT,
-  "weighting" integer,
-  "is_correct" bool NOT NULL
-);
-
--- CREATE TABLE "user_answer" (
---   "id" INT GENERATED ALWAYS AS IDENTITY,
---   "id_user" integer,
---   "id_quiz_test" integer,
---   "id_answer" integer
--- );
+ CREATE TABLE public.user_answer (
+   id INT GENERATED ALWAYS AS IDENTITY,
+   id_user integer,
+   id_quiz_test integer,
+   id_answer integer
+ );
 
 
---
--- CREATE TABLE "user_test" (
---   "id" INT GENERATED ALWAYS AS IDENTITY,
---   "id_test" integer NOT NULL,
---   "id_user" integer NOT NULL,
---   "date_submission" timestamp,
---   "date_started" timestamp,
---   "date_finished" timestamp,
---   "date_abandonned" timestamp
--- );
+
+
+
+ CREATE TABLE public.user_test (
+   id INT GENERATED ALWAYS AS IDENTITY,
+   id_test integer NOT NULL,
+   id_user integer NOT NULL,
+   date_submission timestamp,
+   date_started timestamp,
+   date_finished timestamp,
+   date_abandonned timestamp
+ );
+
+  CREATE TABLE public.notification (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    id_user integer NOT NULL,
+    id_quiz integer NOT NULL,
+    message varchar(250) NOT NULL,
+    type varchar(250) NOT NULL,
+    status varchar(250) NOT NULL,
+    created_at timestamp NOT NULL,
+    updated_at timestamp NOT NULL,
+    is_sent bool NOT NULL
+  );
+
+
+
 
 
 
